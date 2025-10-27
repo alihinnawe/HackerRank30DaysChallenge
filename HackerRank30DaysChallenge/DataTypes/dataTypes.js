0193c6ed3e2c5e451df9811941a0eb9e697a13ee
Ali <hinnaweali@gmail.com>
2025-10-27 16:06:25 +0100

    Added day2 HackerRank30DaysChallenge

---
+++ HackerRank30DaysChallenge/DataTypes/dataTypes.js
@@ -0,0 +1,12 @@
+let var1 = parseInt(readLine());
+let var2 = parseFloat(readLine());
+let var3 = readLine();
+
+// Print the sum of both integer variables on a new line.
+console.log(i + var1);
+
+// Print the sum of the double variables on a new line.
+console.log((d + var2).toFixed(1));
+
+// Concatenate and print the String variables on a new line.
+console.log(s + var3);
