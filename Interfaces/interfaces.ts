9029c18c972d7cb833b9a86a07820744eab74e90
Ali <hinnaweali@gmail.com>
2025-11-04 15:16:16 +0100

    Add a new challenge Interfaces day 19 HckeerRank30DaysChallenge

---
+++ Interfaces/interfaces.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+'use strict';
+interface AdvancedArithmetic {
+    divisorSum(n: number): number;
+}
+class Calculator implements AdvancedArithmetic {
+    divisorSum(n: number): number {
+        let sum = 0;
+        for (let i = 1; i <= n; i++) {
+            if (n % i === 0) sum += i;
+        }
+    return sum;
+    }
+}
+
+function main() {
+    const n: number = Number(readLine().trim());
+    const myCalculator = new Calculator();
+
+    console.log("I implemented: AdvancedArithmetic");
+    console.log(myCalculator.divisorSum(n));
+}
